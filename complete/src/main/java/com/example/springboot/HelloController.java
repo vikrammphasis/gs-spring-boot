3819193af13abcad976a58269af82cdd75cd8fd3
Vikram <vikram.aroskar@mphasis.com>
2021-03-05 06:08:27 +0000

    first cut application_insights

--- complete/src/main/java/com/example/springboot/HelloController.java
+++ complete/src/main/java/com/example/springboot/HelloController.java
@@ -5,9 +5,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HelloController {
+	/*
+	@Autowired
+	TelemetryClient telemetryClient;
+	*/
 
 	@RequestMapping("/")
 	public String index() {
+/*
+		// send event
+		telemetryClient.trackEvent("URI /is triggered");
+
+		// measure DB query benchmark
+		long startTime = System.nanoTime();
+		long endTime = System.nanoTime();
+
+		MetricTelemetry benchmark = new MetricTelemetry();
+		benchmark.setName("Default URL");
+		benchmark.setValue(endTime - startTime);
+		telemetryClient.trackMetric(benchmark);
+*/
 		return "Greetings from Spring Boot!";
 	}
 
